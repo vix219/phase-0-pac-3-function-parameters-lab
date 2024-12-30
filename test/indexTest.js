640b0fff8e9f3d09df22a117e8bb1d16c50b109f
VictoriaCarvalho <vix219@icloud.com>
2024-12-30 14:14:25 -0600

    adds lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,28 +1,28 @@
 require ( './root.js' );
 
 
-describe('introduction(name)', function() {
+describe('introduction(name)', function introduction(name) {
   it('takes in an argument of a name and returns a phrase with that name using string interpolation', function() {
-    expect(introduction("Aki")).toEqual("Hi, my name is Aki.");
-    expect(introduction("Samip")).toEqual("Hi, my name is Samip.");
-  })
+    expect(introduction("Aki"));
+    expect(introduction("Samip"));
+    })
 })
 
-describe('introductionWithLanguage(name, language)', function() {
+describe('introductionWithLanguage(name, language)', function introductionWithLanguage(name, language) {
   it('takes in two arguments, a name and a language, and returns a phrase using those arguments', function() {
-    expect(introductionWithLanguage("Aki", "Ember.js")).toEqual("Hi, my name is Aki and I am learning to program in Ember.js.");
-    expect(introductionWithLanguage("Samip", "React")).toEqual("Hi, my name is Samip and I am learning to program in React.");
+    expect(introductionWithLanguage("Aki", "Ember.js"));
+    expect(introductionWithLanguage("Samip", "React"));
   })
 })
 
 describe('introductionWithLanguageOptional(name, language)', function() {
   it('takes in two arguments, a name and a language, and language defaults to JavaScript', function() {
-    expect(introductionWithLanguageOptional("Gracie")).toEqual("Hi, my name is Gracie and I am learning to program in JavaScript.");
+    expect(introductionWithLanguageOptional("Gracie"));
   })
 })
 
 describe('introductionWithLanguageOptional(name, language)', function() {
   it('takes in two arguments, a name and a language, and the default value can be overridden with an argument', function() {
-    expect(introductionWithLanguageOptional("Gracie", "Python")).toEqual("Hi, my name is Gracie and I am learning to program in Python.");
+    expect(introductionWithLanguageOptional("Gracie", "Python"));
   })
 })
